923490b7d74096d4eb6d820d838b30634ea6af64
Dhamnix <eh.vandaki@gmail.com>
2024-07-02 01:23:28 +0330

    SignUp and Login first

---
+++ src/main/java/org/example/ap_final/SignUpController.java
@@ -0,0 +1,36 @@
+package org.example.ap_final;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SignUpController {
+    @FXML
+    Button LoginButton ;
+    @FXML
+    Button SignUpButton ;
+    @FXML
+    TextField usernameTextField ;
+    @FXML
+    TextField passwordTextField ;
+
+
+    @FXML
+    protected void onLoginButtonClick(ActionEvent e ) throws IOException {
+        Parent backParent = FXMLLoader.load(getClass().getResource("Login-Panel.fxml"));
+        Scene backScene = new Scene(backParent);
+        Stage window = (Stage) SignUpButton.getScene().getWindow();
+        window.setScene(backScene);
+        window.show();
+    }
+    @FXML
+    protected void onSignUpButtonClick(ActionEvent e)  {
+    }
+}
